12a8fe9b7659cd78aff494fc2885fdc1fc2960ec
Carstal <43147122+Carstal@users.noreply.github.com>
2023-08-08 22:57:09 -0400

    comment out non typescript components

--- src/pages/tests/display.tsx
+++ src/pages/tests/display.tsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-interface Props {
-  number: number;
-  name: number;
-}
+// interface Props {
+//   number: number;
+//   name: number;
+// }
 
-const Display: React.FC<Props> = ({ name, number }) => {
-  return (
-    <div className="numberlabel">
-      <h1>{name}</h1>
-      <h2>{number}</h2>
-    </div>
-  );
-};
+// const Display: React.FC<Props> = ({ name, number }) => {
+//   return (
+//     <div className="numberlabel">
+//       <h1>{name}</h1>
+//       <h2>{number}</h2>
+//     </div>
+//   );
+// };
 
-export default Display;
+// export default Display;
