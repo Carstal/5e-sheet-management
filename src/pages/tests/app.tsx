6e063f6792f607154d7b0dde286f67e8bb1100d9
Carstal <43147122+Carstal@users.noreply.github.com>
2023-08-09 10:28:09 -0400

    uncomment test components

--- src/pages/tests/app.tsx
+++ src/pages/tests/app.tsx
@@ -1,43 +1,40 @@
-// import React, { useState, useEffect } from "react";
-// import Counter from "./counter";
-// import Display from "./display";
+import React, { useState, useEffect } from "react";
+import Counter from "./counter";
+import Display from "./display";
 
-// function App() {
-//   const [cha, setCha] = useState(3);
-//   const [chaEven, setChaEven] = useState(cha % 2 === 0);
-//   const [int, setInt] = useState(5);
-//   const [intEven, setIntEven] = useState(int % 2 === 0);
-//   const [str, setStr] = useState(8);
-//   const [strEven, setStrEven] = useState(str % 2 === 0);
+function App() {
+  const [cha, setCha] = useState(3);
+  const [chaEven, setChaEven] = useState(cha % 2 === 0);
+  const [int, setInt] = useState(5);
+  const [intEven, setIntEven] = useState(int % 2 === 0);
+  const [str, setStr] = useState(8);
+  const [strEven, setStrEven] = useState(str % 2 === 0);
 
-//   useEffect(() => {
-//     // Check if the count value is even or odd and update isEven accordingly.
-//     setChaEven(cha % 2 === 0);
-//     setCha(cha);
-//   }, [cha]);
+  useEffect(() => {
+    setChaEven(cha % 2 === 0);
+    setCha(cha);
+  }, [cha]);
 
-//   useEffect(() => {
-//     // Check if the count value is even or odd and update isEven accordingly.
-//     setIntEven(int % 2 === 0);
-//     setInt(int);
-//   }, [int]);
+  useEffect(() => {
+    setIntEven(int % 2 === 0);
+    setInt(int);
+  }, [int]);
 
-//   useEffect(() => {
-//     // Check if the count value is even or odd and update isEven accordingly.
-//     setStrEven(str % 2 === 0);
-//     setStr(str);
-//   }, [str]);
+  useEffect(() => {
+    setStrEven(str % 2 === 0);
+    setStr(str);
+  }, [str]);
 
-//   return (
-//     <div className="App">
-//       <Counter number={cha} />
-//       <Counter number={int} />
-//       <Counter number={str} />
-//       <Display name="Charisma" number={cha} />
-//       <Display name="Intelligence" number={int} />
-//       <Display name="Strength" number={str} />
-//     </div>
-//   );
-// }
+  return (
+    <div className="App">
+      <Counter number={cha} />
+      <Counter number={int} />
+      <Counter number={str} />
+      <Display name="Charisma" number={cha} />
+      <Display name="Intelligence" number={int} />
+      <Display name="Strength" number={str} />
+    </div>
+  );
+}
 
-// export default App;
+export default App;
--- src/pages/tests/counter.tsx
+++ src/pages/tests/counter.tsx
@@ -1,35 +1,35 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-// interface Props {
-//   number: number;
-// }
+interface Props {
+  number: number;
+}
 
-// const Counter: React.FC<Props> = ({ number }) => {
-//   const [count, setCount] = useState(number);
-//   const [isEven, setIsEven] = useState(false);
+const Counter: React.FC<Props> = ({ number }) => {
+  const [count, setCount] = useState(number);
+  const [isEven, setIsEven] = useState(false);
 
-//   const increment = () => {
-//     setCount(count + 1);
-//   };
+  const increment = () => {
+    setCount(count + 1);
+  };
 
-//   const decrement = () => {
-//     setCount(count - 1);
-//   };
+  const decrement = () => {
+    setCount(count - 1);
+  };
 
-//   useEffect(() => {
-//     // Check if the count value is even or odd and update isEven accordingly.
-//     setIsEven(count % 2 === 0);
-//   }, [count]);
+  useEffect(() => {
+    // Check if the count value is even or odd and update isEven accordingly.
+    setIsEven(count % 2 === 0);
+  }, [count]);
 
-//   return (
-//     <div className="counter">
-//       <h1>Simple Counter</h1>
-//       <p>Count: {count}</p>
-//       <p>{isEven ? "Even" : "Odd"}</p>
-//       <button onClick={increment}>Increment</button>
-//       <button onClick={decrement}>Decrement</button>
-//     </div>
-//   );
-// };
+  return (
+    <div className="counter">
+      <h1>Simple Counter</h1>
+      <p>Count: {count}</p>
+      <p>{isEven ? "Even" : "Odd"}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
 
-// export default Counter;
+export default Counter;
